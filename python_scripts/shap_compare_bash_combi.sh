493eb86b86f4d9093f1a4df0ddefe3a669facd65
CatNoHat <catherinelong2@gmail.com>
2026-02-24 11:49:24 +0000

    adding all graphs

---
+++ python_scripts/shap_compare_bash_combi.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+optimization_algorithms=("Random_Search_optimizer" "Grid_Search_optimizer")
+descriptors=("SMILES" "RDKit" "Bit_Morgan")
+
+for optimization_algorithm in "${optimization_algorithms[@]}"; do
+	        for descriptor in "${descriptors[@]}"; do
+			sed -i "s/^[[:space:]]*descriptor = .*/descriptor = (\"$descriptor\",)/" backup_config.py
+		        log_file="LowerFlam_${descriptor}_${optimization_algorithm}_compare_MODELS.log"
+			nohup python compare_models.py > "$log_file" 2>&1 &
+
+			wait $!
+	                echo "completed all LowerFlam runs with models for $descriptor and ${optimization_algorithm} Output saved to $log_file"
+
+		done
+done
+
+models=("SVR" "Random_Forest" "XGBoost" "Lasso")
+descriptors=("SMILES" "RDKit" "Bit Morgan")
+optimization_algorithms=("Random_Search_optimizer" "Grid_Search_optimizer")
+properties=("boiling_point" "Lower_Flam" "enthalpy_of_vaporization" "flashpoint" "Viscosity_as_logVis")
+
+for property in "${properties[@]}"; do
+	        for model in "${models[@]}"; do
+			               for descriptor in "${descriptors[@]}"; do 
+					       sed -i "s/^[[:space:]]*property = .*/property = (\"$property\",)/" backup_config.py
+					       sed -i "s/^[[:space:]]*model = .*/model = (\"$model\",)/" backup_config.py
+
+					       sed -i "s/^[[:space:]]*descriptor = .*/descriptor = (\"$descriptor\",)/" backup_config.py
+
+					       log_file="${property}_shap_${optimization_algorithm}_${model}_${descriptor}_V1.log"
+
+																	                          			           nohup python shap_analysis.py > "$log_file" 2>&1 &
+																								   wait $!
+
+																							           echo "completed all $property runs with model $model , $descriptor and $optimization_algorithm . Output saved to $log_file"
+																					                    done
+		done
+done	
